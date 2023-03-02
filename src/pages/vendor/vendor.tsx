102454e33bf26f5f49b5a9e6a1cbc52c5a42d9ea
bangaradi <adityavb@cse.iitk.ac.in>
2023-03-02 18:51:13 +0530

    add SecondSidebar functionality, on customer side

--- src/pages/vendor/vendor.tsx
+++ src/pages/vendor/vendor.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './Overview'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('groot') as HTMLElement).render(
