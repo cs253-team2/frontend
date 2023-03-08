0a31a3cbe58f6c27f766b1bbd7da16a62daec279
RajaniAS <100557346+RajaniAS@users.noreply.github.com>
2023-03-09 03:48:48 +0530

    vendor page(for customer) created

--- src/pages/customer/Vendors.tsx
+++ src/pages/customer/Vendors.tsx
@@ -1,84 +1,84 @@
-import * as React from 'react';
-import { CssVarsProvider } from '@mui/joy/styles';
-import GlobalStyles from '@mui/joy/GlobalStyles';
-import CssBaseline from '@mui/joy/CssBaseline';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import Breadcrumbs from '@mui/joy/Breadcrumbs';
-import Link from '@mui/joy/Link';
-import Typography from '@mui/joy/Typography';
-import useScript from './useScript';
-import FirstSidebar from './components/FirstSidebar';
-import SecondSidebar from './components/SecondSidebar';
-import OrderTable from './components/OrderTable';
-import Header from './components/Header';
-import ColorSchemeToggle from './components/ColorSchemeToggle';
-import customTheme from './theme';
-
-
+import * as React from "react";
+import { CssVarsProvider } from "@mui/joy/styles";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Breadcrumbs from "@mui/joy/Breadcrumbs";
+import Link from "@mui/joy/Link";
+import Typography from "@mui/joy/Typography";
+import useScript from "./useScript";
+import FirstSidebar from "./components/FirstSidebar";
+import SecondSidebar from "./components/SecondSidebar";
+import Header from "./components/Header";
+import ColorSchemeToggle from "./components/ColorSchemeToggle";
+import customTheme from "./theme";
+import VendorTable2 from "./components/VendorTable2";
 export default function App() {
-    return (
-        <div>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Breadcrumbs
-              size="sm"
-              aria-label="breadcrumbs"
-              separator={<i data-feather="chevron-right" />}
-              sx={{
-                '--Breadcrumbs-gap': '1rem',
-                '--Icon-fontSize': '16px',
-                fontWeight: 'lg',
-                color: 'neutral.400',
-                px: 0,
-              }}
-              
-            >
-              <Link
-                underline="none"
-                color="neutral"
-                fontSize="inherit"
-                href="#some-link"
-                aria-label="Home"
-              >
-                <i data-feather="home" />
-              </Link>
-              <Link
-                underline="hover"
-                color="neutral"
-                fontSize="inherit"
-                href="#some-link"
-              >
-                Dashboard
-              </Link>
-              <Typography fontSize="inherit" variant="soft" color="primary">
-                Vendors
-              </Typography>
-            </Breadcrumbs>
-            <ColorSchemeToggle
-              sx={{ ml: 'auto', display: { xs: 'none', md: 'inline-flex' } }}
-            />
-          </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              my: 1,
-              gap: 1,
-              flexWrap: 'wrap',
-              '& > *': {
-                minWidth: 'clamp(0px, (500px - 100%) * 999, 100%)',
-                flexGrow: 1,
-              },
-            }}
+  return (
+    <div>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Breadcrumbs
+          size="sm"
+          aria-label="breadcrumbs"
+          separator={<i data-feather="chevron-right" />}
+          sx={{
+            "--Breadcrumbs-gap": "1rem",
+            "--Icon-fontSize": "16px",
+            fontWeight: "lg",
+            color: "neutral.400",
+            px: 0,
+          }}
+        >
+          <Link
+            underline="none"
+            color="neutral"
+            fontSize="inherit"
+            href="#some-link"
+            aria-label="Home"
           >
-            <Typography level="h1" fontSize="xl4">
-              Vendors
-            </Typography>
-            <Box sx={{ flex: 999 }} />
-           
-          </Box>
-        </div>
-        
-
-    )
+            <i data-feather="home" />
+          </Link>
+          <Link
+            underline="hover"
+            color="neutral"
+            fontSize="inherit"
+            href="#some-link"
+          >
+            Dashboard
+          </Link>
+          <Typography fontSize="inherit" variant="soft" color="primary">
+            Vendors
+          </Typography>
+        </Breadcrumbs>
+        <ColorSchemeToggle
+          sx={{ ml: "auto", display: { xs: "none", md: "inline-flex" } }}
+        />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          my: 1,
+          gap: 1,
+          flexWrap: "wrap",
+          "& > *": {
+            minWidth: "clamp(0px, (500px - 100%) * 999, 100%)",
+            flexGrow: 1,
+          },
+        }}
+      >
+        <Typography level="h1" fontSize="xl4">
+          Vendors
+        </Typography>
+        <Box sx={{ flex: 999 }} />
+        <Button color="success" href="#contained-buttons">
+          Add Vendor
+        </Button>
+      </Box>
+      <Box>
+        <VendorTable2/>
+      </Box>
+    </div>
+  );
 }
