49ebee8233316597a404c1a32ced74251cfca8f5
bangaradi <adityavb@cse.iitk.ac.in>
2023-03-20 01:50:44 +0530

    add dues page api to vendor

--- src/pages/vendor/AllDues.tsx
+++ src/pages/vendor/AllDues.tsx
@@ -15,110 +15,119 @@ import Header from './components/Header';
 import ColorSchemeToggle from './components/ColorSchemeToggle';
 import customTheme from './theme';
 import DuesTable from './components/DuesTable';
+import {PendingDue, getPendingDues} from "../callbacks/VendorDues";
 
-const rows = [
-  {
-    id: 'INV-1234',
-    date: 'Jan 3, 2023',
-    status: 'Paid',
-    vendor: {
-      initial: 'O',
-      name: 'Olivia Ryhe',
-      email: 'olivia@email.com',
-    },
-    amount: '69',
-  },
-  {
-    id: 'INV-1233',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    vendor: {
-      initial: 'S',
-      name: 'Steve Hampton',
-      email: 'steve.hamp@email.com',
-    },
-    amount: '420',
-  },
-  {
-    id: 'INV-1232',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    vendor: {
-      initial: 'C',
-      name: 'Ciaran Murray',
-      email: 'ciaran.murray@email.com',
-    },
-    amount: '69',
-  },
-  {
-    id: 'INV-1231',
-    date: 'March 3, 2023',
-    status: 'Refunded',
-    vendor: {
-      initial: 'M',
-      name: 'Maria Macdonald',
-      email: 'maria.mc@email.com',
-    },
-    amount: '69',
-  },
-  {
-    id: 'INV-1230',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    vendor: {
-      initial: 'C',
-      name: 'Charles Fulton',
-      email: 'fulton@email.com',
-    },
-    amount: '69',
-  },
-  {
-    id: 'INV-1229',
-    date: 'Dec 3, 2023',
-    status: 'Cancelled',
-    vendor: {
-      initial: 'J',
-      name: 'Jay Hooper',
-      email: 'hooper@email.com',
-    },
-    amount: '69',
-  },
-  {
-    id: 'INV-1228',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    vendor: {
-      initial: 'K',
-      name: 'Krystal Stevens',
-      email: 'k.stevens@email.com',
-    },
-    amount: '420',
-  },
-  {
-    id: 'INV-1227',
-    date: 'Feb 3, 2023',
-    status: 'Paid',
-    vendor: {
-      initial: 'S',
-      name: 'Sachin Flynn',
-      email: 's.flyn@email.com',
-    },
-    amount: '420',
-  },
-  {
-    id: 'INV-1226',
-    date: 'Feb 3, 2023',
-    status: 'Cancelled',
-    vendor: {
-      initial: 'B',
-      name: 'Bradley Rosales',
-      email: 'brad123@email.com',
-    },
-    amount: '420',
-  },
-];
+// const rows = [
+//   {
+//     id: 'INV-1234',
+//     date: 'Jan 3, 2023',
+//     status: 'Paid',
+//     vendor: {
+//       initial: 'O',
+//       name: 'Olivia Ryhe',
+//       email: 'olivia@email.com',
+//     },
+//     amount: '69',
+//   },
+//   {
+//     id: 'INV-1233',
+//     date: 'Feb 3, 2023',
+//     status: 'Paid',
+//     vendor: {
+//       initial: 'S',
+//       name: 'Steve Hampton',
+//       email: 'steve.hamp@email.com',
+//     },
+//     amount: '420',
+//   },
+//   {
+//     id: 'INV-1232',
+//     date: 'Feb 3, 2023',
+//     status: 'Paid',
+//     vendor: {
+//       initial: 'C',
+//       name: 'Ciaran Murray',
+//       email: 'ciaran.murray@email.com',
+//     },
+//     amount: '69',
+//   },
+//   {
+//     id: 'INV-1231',
+//     date: 'March 3, 2023',
+//     status: 'Refunded',
+//     vendor: {
+//       initial: 'M',
+//       name: 'Maria Macdonald',
+//       email: 'maria.mc@email.com',
+//     },
+//     amount: '69',
+//   },
+//   {
+//     id: 'INV-1230',
+//     date: 'Feb 3, 2023',
+//     status: 'Paid',
+//     vendor: {
+//       initial: 'C',
+//       name: 'Charles Fulton',
+//       email: 'fulton@email.com',
+//     },
+//     amount: '69',
+//   },
+//   {
+//     id: 'INV-1229',
+//     date: 'Dec 3, 2023',
+//     status: 'Cancelled',
+//     vendor: {
+//       initial: 'J',
+//       name: 'Jay Hooper',
+//       email: 'hooper@email.com',
+//     },
+//     amount: '69',
+//   },
+//   {
+//     id: 'INV-1228',
+//     date: 'Feb 3, 2023',
+//     status: 'Cancelled',
+//     vendor: {
+//       initial: 'K',
+//       name: 'Krystal Stevens',
+//       email: 'k.stevens@email.com',
+//     },
+//     amount: '420',
+//   },
+//   {
+//     id: 'INV-1227',
+//     date: 'Feb 3, 2023',
+//     status: 'Paid',
+//     vendor: {
+//       initial: 'S',
+//       name: 'Sachin Flynn',
+//       email: 's.flyn@email.com',
+//     },
+//     amount: '420',
+//   },
+//   {
+//     id: 'INV-1226',
+//     date: 'Feb 3, 2023',
+//     status: 'Cancelled',
+//     vendor: {
+//       initial: 'B',
+//       name: 'Bradley Rosales',
+//       email: 'brad123@email.com',
+//     },
+//     amount: '420',
+//   },
+// ];
 
 export default function App() {
+  const [rows, setRows] = React.useState<PendingDue[]>([]);
+
+
+  React.useEffect(() => {
+      getPendingDues().then((data: PendingDue[]) => {
+          setRows(data);
+      });
+  }, []);
     return (
         <div>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
